3b977f9869f9e7e1c4254ea0c4fed98e43e28584
dividedbyzero360 <wastabirrajveealamsa_cse10@its.edu.in>
2020-11-29 18:52:39 -0500

    Building index page to show whether the user is signed in or not

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,6 +4,7 @@ import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
+  console.log('Inside auth microservice currentuser');
   res.send({ currentUser: req.currentUser || null });
 });
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,33 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
   return <h1>Hello World</h1>;
 };
+
+// This getIntialProps won't work if we have a getInitialProps in the _app.js page
+// This method is mostly called from next.js server but in one particular scenario
+//(When we came to this page by clicking on an url while on the app) it is called from the browser
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    console.log('We are making a request from the server');
+    // request should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local
+    // We get the url by 1. kubectl get namespace 2. kubectl get services -n ingress-nginx (= ingress-nginx-controller [The one that is of type load balancer])
+
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    return data;
+  } else {
+    console.log('We are making a request from the browser');
+    // request should be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+};
+
+export default LandingPage;
