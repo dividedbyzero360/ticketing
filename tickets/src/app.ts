86f20742c8a9e7f5a4f8dea7db1c1a8291ee9b8c
dividedbyzero360 <wastabirrajveealamsa_cse10@its.edu.in>
2020-12-02 23:55:01 -0500

    Setup  infra for ticketing microservice

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
 import { errorHandler, NotFoundError } from '@walam/common';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
@@ -22,10 +18,6 @@ app.use(
   })
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
